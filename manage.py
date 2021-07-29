14f4fc451506f3d907b13f192db36b9e36cc3487
Raffael Dieske <r.dieske@outlook.de>
2021-07-29 23:42:32 +0200

    Enable remote debugging and test running

--- manage.py
+++ manage.py
@@ -2,10 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import ptvsd
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ionos.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ionos.settings')   
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,6 +15,15 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+      
+    if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):  
+        ptvsd_port = 3000 
+        try:
+            ptvsd.enable_attach(address=("0.0.0.0", ptvsd_port))
+            print("Started ptvsd at port %s." % ptvsd_port)
+        except OSError:
+            print("ptvsd port %s already in use." % ptvsd_port)
+          
     execute_from_command_line(sys.argv)
 
 
